827fe079bcf8ddd59e462c13bac09677eea9fdb5
heer rashiya <heerashiya@gmail.com>
2024-08-21 17:24:41 +0530

    dice

--- lecture15/dice.js
+++ lecture15/dice.js
@@ -3,10 +3,9 @@ function rollDice() {
     
     const dice = document.getElementById('dice');
 
-    const diceRoll = Math.floor(Math.random() * 6) + 1;
+    const diceRoll = Math.floor(Math.random() * 5) + 1;
 
     const diceNumbers = ['1', '2', '3', '4', '5', '6'];
 
     dice.textContent = diceNumbers[diceRoll - 1];
 }
-document.getElementById('rollButton').addEventListener('click', rollDice);
