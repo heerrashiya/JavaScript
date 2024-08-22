c094a86e503f5e65e472607bd57b611131be274a
heer rashiya <heerashiya@gmail.com>
2024-08-22 10:31:00 +0530

    instapage

--- lecture15/dice.js
+++ lecture15/dice.js
@@ -3,7 +3,7 @@ function rollDice() {
     
     const dice = document.getElementById('dice');
 
-    const diceRoll = Math.floor(Math.random() * 5) + 1;
+    const diceRoll = Math.floor(Math.random() * 6) + 1;
 
     const diceNumbers = ['1', '2', '3', '4', '5', '6'];
 
