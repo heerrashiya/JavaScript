f0b930d453ddbaa08ae38a7aca4d78965495b707
heer rashiya <heerashiya@gmail.com>
2024-09-04 17:42:09 +0530

    project

--- Project/components/helper.js
+++ Project/components/helper.js
@@ -6,3 +6,16 @@ const getValue=(className)=>
 }
 
 export default getValue
+
+export const createTag = (tag, value) => {
+    let tagName = document.createElement(tag);
+    if (tag == "img") {
+        tagName.src = value;
+    }
+    else {
+        tagName.innerHTML = value;
+    }
+
+
+    return tagName;
+};
---
+++ Project/js/addproducts.js
@@ -0,0 +1,31 @@
+
+import getValue from "../components/helper.js";
+import Navbar from "../components/navbar.js";
+
+document.getElementById("navbar").innerHTML = Navbar();
+
+let isLogin = localStorage.getItem("isLogin") || false;
+
+if (isLogin == false) {
+    window.location.href = "./login.html";
+}
+
+
+let products = JSON.parse(localStorage.getItem("products")) || [];
+
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let product = {
+        title: getValue(".title"),
+        price: getValue(".price"),
+        category: getValue(".category"),
+        img: getValue(".img"),
+    };
+    products.push(product);
+    localStorage.setItem("products", JSON.stringify(products));
+
+};
+
+document.getElementById("productdata").addEventListener("submit", handleSubmit);
