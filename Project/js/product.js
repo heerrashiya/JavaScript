bb2260f6dc4e6db7ea59bbcab7b3ba4c7100b529
heer rashiya <heerashiya@gmail.com>
2024-09-05 14:37:06 +0530

    project

--- Project/js/product.js
+++ Project/js/product.js
@@ -5,22 +5,24 @@ document.getElementById("navbar").innerHTML = Navbar();
 
 let products = JSON.parse(localStorage.getItem("products")) || [];
 
-
-
 const mapper = (data) => {
+    if (data.length === 0) {
+        document.getElementById("productList").textContent = "No products available.";
+        return;
+    }
 
-    data.map((ele) => {
+    data.forEach((ele) => {
+        let img = createTag("img", "", { src: ele.img, alt: ele.title });
+        let title = createTag("h3", ele.title);
+        let price = createTag("p", `Price: $${ele.price}`);
+        let category = createTag("p", `Category: ${ele.category}`);
+        
+        let div = document.createElement("div");
+        div.classList.add("product-item");
+        div.append(img, title, price, category);
 
-        let img = createTag("img", ele.img)
-        let price = createTag("p", ele.price)
-        let title = createTag("h3", ele.title)
-        let category = createTag("p", ele.category)
-        let div = document.createElement("div")
-        div.append(img, title, price, category)
-        document.getElementById("productList").append(div)
-    })
+        document.getElementById("productList").append(div);
+    });
+};
 
-}
-
-
-mapper(products)
+mapper(products);
