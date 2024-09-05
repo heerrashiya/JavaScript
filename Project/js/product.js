07f954c526b6305f7bd073631f5c1237570020f4
heer rashiya <heerashiya@gmail.com>
2024-09-05 15:55:03 +0530

    project

--- Project/js/product.js
+++ Project/js/product.js
@@ -1,4 +1,5 @@
-import { createTag } from "../components/helper.js";
+
+import getValue, { createTag } from "../components/helper.js";
 import Navbar from "../components/navbar.js";
 
 document.getElementById("navbar").innerHTML = Navbar();
@@ -6,23 +7,18 @@ document.getElementById("navbar").innerHTML = Navbar();
 let products = JSON.parse(localStorage.getItem("products")) || [];
 
 const mapper = (data) => {
-    if (data.length === 0) {
-        document.getElementById("productList").textContent = "No products available.";
-        return;
-    }
-
-    data.forEach((ele) => {
-        let img = createTag("img", "", { src: ele.img, alt: ele.title });
+    document.getElementById("productlist").innerHTML = "";
+    data.map((ele) => {
+        let img = createTag("img", ele.img);
+        let price = createTag("p", ele.price);
         let title = createTag("h3", ele.title);
-        let price = createTag("p", `Price: $${ele.price}`);
-        let category = createTag("p", `Category: ${ele.category}`);
-        
+        let category = createTag("p", ele.category);
+        let buyBtn = createTag("button", "Buy");
         let div = document.createElement("div");
-        div.classList.add("product-item");
-        div.append(img, title, price, category);
-
-        document.getElementById("productList").append(div);
+        div.append(img, title, price, category, buyBtn);
+        document.getElementById("productlist").append(div);
     });
 };
 
 mapper(products);
+
