2bfda29658d94e29faa1e5cb59afbb9a07d5769b
heer rashiya <heerashiya@gmail.com>
2024-09-16 19:44:25 +0530

    student

---
+++ Student/navbar.js
@@ -0,0 +1,35 @@
+
+
+const Navbar = () => {
+    let islogin=localStorage.getItem("islogin") || false
+    let username=localStorage.getItem("username")
+      return `
+     <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="/Project/home.html">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/Project/page/product.html">Add</a>
+                </li> 
+              </ul>
+                  <form class="d-flex" role="search" id="searching">
+                  <input class="form-control me-2"type="search" placeholder="Search" aria-label="Search" id="search"/>
+                  <button class="btn btn-outline-success" type="submit">
+                    Search
+                  </button>
+                </form>
+            </div>
+          </div>
+        </nav>
+        `
+    }
+    
+    export default Navbar
+    
---
+++ Student/student.js
@@ -0,0 +1,5 @@
+import Navbar from "./navbar.js";
+
+document.getElementById("htl").addEventListener("click",()=>handleshort("htl"));
+
+document.getElementById("navbar").innerHTML=Navbar()
